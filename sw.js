2c605020352887a87946189b6bc7c141ac45d662
satishkumar <mandapallisatish64@gmail.com>
2019-10-04 11:41:44 -0500

    ADDED CHANGES

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
-let deferredPrompt;
+//let deferredPrompt;
 
 // window.addEventListener('beforeinstallprompt', (e) => {
 //   // Stash the event so it can be triggered later.
@@ -43,22 +43,22 @@ let deferredPrompt;
 // window.addEventListener('appinstalled', (evt) => {
 //   console.log('a2hs installed');
 // });
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-});
+// btnAdd.addEventListener('click', (e) => {
+//   // hide our user interface that shows our A2HS button
+//   btnAdd.style.display = 'none';
+//   // Show the prompt
+//   deferredPrompt.prompt();
+//   // Wait for the user to respond to the prompt
+//   deferredPrompt.userChoice
+//     .then((choiceResult) => {
+//       if (choiceResult.outcome === 'accepted') {
+//         console.log('User accepted the A2HS prompt');
+//       } else {
+//         console.log('User dismissed the A2HS prompt');
+//       }
+//       deferredPrompt = null;
+//     });
+// });
 self.addEventListener('activate', function(event) {
 
   var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
